90d8d0022b91592304b158dd150e35abe863b204
shab20 <shab_93@hotmail.com>
2023-01-26 17:45:45 +0400

    Done

---
+++ main1.js
@@ -0,0 +1,15 @@
+let newDog = "Chihuahua";
+
+console.log (newDog.indexOf())
+
+console.log(newDog.toUpperCase())
+console.log(newDog.toLowerCase())
+
+if(newDog ==="chihuahua"){
+    console.log ("I love Chihuahuas, it's my favorite dog breed")
+
+}
+else{
+    console.log(" 'I dont care, I prefer cats' ")
+    
+}
---
+++ main2.js
@@ -0,0 +1,9 @@
+const number = prompt("enter a number");
+
+
+if(Number (number) % 2 === 0){
+    console.log(number + "is an even number")
+
+} else {
+    console.log(number + "is an odd number")
+}
